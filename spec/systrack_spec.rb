34e5c54cf60fe9260437fc2176c607969023a1ce
vedasara <vedasara.bvks@gmail.com>
2012-12-02 17:19:35 +0100

    spec extract date

--- spec/systrack_spec.rb
+++ spec/systrack_spec.rb
@@ -2,14 +2,22 @@ require './lib/systrack'
 
 describe "track system" do
 	before :all do
-    	@fake_uptime = "13:06:08 up  1:29,  1 user,  load average: 0.03, 0.06, 0.06"
-    	@system = SystemTrack.new
+  	@fake_uptime = "13:06:08 up  1:29,  1 user,  load average: 0.03, 0.06, 0.06"
+  	@fake_date = "Sun Dec  2 17:10:06 CET 2012"
+  	@system = SystemTrack.new
 	end
+
 	before :each do
-    	@system.stub(:uptime).and_return(@fake_uptime)
+  	@system.stub(:uptime).and_return(@fake_uptime)
+  	@system.stub(:date).and_return(@fake_date)
   	end
 
 	it "should extract average load" do
 		@system.load_average.should == "0.03"
 	end
+
+	it "should extract date" do
+		@system.extract_date.should == "Sun Dec  2"
+	end
+  
 end
